9d6412d6fcd16e3371801577e9b631aaeb7cfc38
raulfarce01 <raulito72001@gmail.com>
2022-05-31 21:54:24 +0200

    Intento conexión

--- web/controller/apiConecta.php
+++ web/controller/apiConecta.php
@@ -8,6 +8,7 @@
         $array = [];
         $key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE4MTIiLCJuYmYiOjE2NTIzNjA3MDYsImV4cCI6MTY1NDk1MjcwNiwiaWF0IjoxNjUyMzYwNzA2fQ.ApUPWyUu7VIUY1Sd5Hkr1fs3aJD1WI0PH4yReJ_Cpic"
         ;
+        //$key = "STmF2J2CfXaCVfe56MoRoQc4oyeu9SvomTVKmHWYEgKRA2wmgMS4DDswBnQPU09";
         $query = $_GET['query'];
         $page = $_GET['page'];
         //$link =  "https://api.aniapi.com/v1/user_story"; 
@@ -26,7 +27,9 @@
         
         $response = curl_exec($ch);
 
-        //$data = isset($response) ? json_decode($response) : "Negativo";
+        //echo json_decode($response);
+        //var_dump($response);
+        $data = isset($response) ? json_decode($response) : "Negativo";
         //echo json_decode($data);
         //var_dump($data);
         //var_dump($response);
